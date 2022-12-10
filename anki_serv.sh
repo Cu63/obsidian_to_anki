1302a40e89ba73827226cb75c30f78b79f097062
Cu63 <scube.str@gmail.com>
2022-12-10 21:07:53 +0300

    del sh file

--- anki_serv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-defaults write net.ankiweb.dtop NSAppSleepDisabled -bool true
-defaults write net.ichi2.anki NSAppSleepDisabled -bool true
-defaults write org.qt-project.Qt.QtWebEngineCore NSAppSleepDisabled -bool true
