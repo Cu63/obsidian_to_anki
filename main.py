15008a8576460899093b31d49895d8a17b8d940e
Cu63 <scube.str@gmail.com>
2022-12-11 23:23:33 +0300

    add help meny

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from anki_api import add_card
 from obsidian_parser import create_cards
 import os
+import sys
 
 
-def main():
+def get_path():
     try:
         file = open('.config', 'r')
         path = file.readline().strip()
@@ -16,6 +17,25 @@ def main():
         path = input('Write path to obsidian cards: ')
         with open('.config', 'w') as f:
             f.write(f'path={path}')
+    return path
+
+
+
+def main():
+    if len(sys.argv) == 2:
+        if sys.argv[1].startswith('--path=') or sys.argv[1].startswith('-p='):
+            with open('.config', 'w') as f:
+                path = sys.argv[1].split("=", maxsplit=1)[1]
+                f.write(f'path={path}')
+        elif sys.argv[1] == '--test' or sys.argv[1] == '-t':
+            path = './test_cards'
+        elif sys.argv[1] == '--help' or sys.argv[1] == '-h':
+            print('\t <--test/-t>="test program')
+            print('\t <--path/-p>="path to cards"')
+            print('\t <--help/-h>="for help"')
+            return
+    else:
+        path = get_path()
 
     files = os.listdir(path)
     for file in files:
